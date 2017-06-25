c5a7f8dba4b3a14e604e85388920ede38bd67275
osa929 <osa929@gmail.com>
2017-06-25 10:34:07 +0000

    DIVE08　ログイン機能対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- 
-  get 'top/index'
 
+  devise_for :users
+  
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
