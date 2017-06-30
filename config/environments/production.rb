d6f4a82021e67854f63bc767923f2e6e13c9f1f5
長船託也 <osafune@osa929Mac.local>
2017-06-30 19:44:02 +0900

    DIVE11 herokuエラー対応3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'stark-brook-83272.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
